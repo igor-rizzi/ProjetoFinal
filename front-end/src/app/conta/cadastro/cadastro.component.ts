eec52641593e20d2e9c0cb8c2187764da66bb992
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2023-05-24 21:30:20 -0300

    criação da parte de cadastro de usuários

--- front-end/src/app/conta/cadastro/cadastro.component.ts
+++ front-end/src/app/conta/cadastro/cadastro.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { DisplayMessage, GenericValidator, ValidationMessages } from './../../utils/generic-form-validation';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioModel } from '../models/usuario.model';
+import { ContaService } from '../services/conta.service';
 
 @Component({
   selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit, AfterViewInit {
+  cadastroForm: FormGroup;
+  usuario: UsuarioModel;
 
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private contaService: ContaService)
+  {
+    this.validationMessages = {
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmPassword: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      },
+    };
+  }
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', []],
+      confirmPassword: ['']
+    });
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  adicionarConta(){
+    if (this.cadastroForm.dirty && this.cadastroForm.valid){
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+
+      this.contaService.registrarUsuario(this.usuario);
+    }
+  }
 }
